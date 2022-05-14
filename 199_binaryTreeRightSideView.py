5dbc731a5b9f028b684cdbf3b97001d48f9bf9ad
chengjiepeng <chengjiepeng@omusic.com.tw>
2022-05-14 16:36:51 +0800

    #199
    traversal first for right, and save the first level value of node

---
+++ 199_binaryTreeRightSideView.py
@@ -0,0 +1,48 @@
+from typing import Optional, List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    def __repr__(self):
+        returnString = "TreeNode{val:"
+        if (self.val):
+            returnString += str(self.val)
+        else:
+            returnString += "None"
+
+        returnString += ",left:"
+
+        if(self.left):
+            returnString += str(self.left)
+        else:
+            returnString += "None"
+
+        returnString += ",rignt:"
+
+        if(self.right):
+            returnString += str(self.right)
+        else:
+            returnString += "None"
+
+        returnString += "}"
+
+        return returnString
+
+class Solution:
+    def __init__(self):
+        self.returnList = []
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        self.traversal(root, 0)
+        return self.returnList
+    def traversal(self, root, level)-> None:
+        if root is None:
+            return
+        if (len(self.returnList)) < level + 1:
+            self.returnList.append(root.val)
+        self.traversal(root.right, level + 1)
+        self.traversal(root.left, level + 1)
+
+
+solution = Solution()
+print(solution.preorderTraversal(TreeNode(4, TreeNode(9, TreeNode(5), TreeNode(1)), TreeNode(0))))
